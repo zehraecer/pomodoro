0e24e4b7b910606d64ac6f43055ad585ea98958f
Zehra Ecer <zehraecer1996@hotmail.com>
2024-07-07 12:55:23 +0300

    settimeout 1000 yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           setMinute(minute => minute - 1)
           setSecond(59)
         }
-      }, 100)
+      }, 1000)
     if (minute === 0 || second === 0) {
       return
     }
